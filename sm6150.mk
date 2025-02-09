6b64a90ffb9784aa83f0653efcce65b00b78b766
Dhina17 <dhinalogu@gmail.com>
2025-02-09 17:21:53 +0100

    sm6150-common: Implement torch light control
    [RealJohnGalt:] modify for oneplus usage, add toggle switch
    [cyberknight777]: Adapt nodes and SEPolicy for sunny
    
    Change-Id: I0b86fbeefbe3b951c5b9827a06c066dd71ada002
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: clarencelol <clarencekuiek@proton.me>

--- sm6150.mk
+++ sm6150.mk
@@ -90,6 +90,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 $(call soong_config_set_bool,camera,override_format_from_reserved,true)
+$(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.sm6150)
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
