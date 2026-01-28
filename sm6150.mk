44df75c4c90df21cd65f77f63aa867f336944736
K R I S H N A <krishnakripa34567@gmail.com>
2026-01-28 06:06:43 +0000

    sm6150-common: Support for XiaomiDolby
    * Let's get some Atmos magic for our sm6150 family
    Signed-off-by: K R I S H N A <pure-soul-kk@proton.me>
    Signed-off-by: therealmharc <therealmharc@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -28,6 +28,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
+# Inherit from Dolby
+$(call inherit-product-if-exists, hardware/dolby/dolby.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
