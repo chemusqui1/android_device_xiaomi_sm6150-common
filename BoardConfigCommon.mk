dfc4d0c4c08ee58d6d7876f690aa31b7d22c86c7
chemusqui1 <jmportos@gmail.com>
2026-05-22 23:48:34 +0200

    sm6150-common: Commonize SPL level
    * There are some devices using this common tree which were marked as EOL by the OEM that got up to either a less recent SPL update, or an inferior Android version (or both), retaining the common platform BSP/kernel as the newer ones.
    
    * As common vendor blobs got updated from latest device with support with a way recent SPL update on vendor side, for some device-specific vendor blobs (camera libs/firmware...), doesn't make sense to keep this device-specific. Just get in pair with the common, the most important ones
    
    Signed-off-by: chemusqui1 <jmportos@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -128,6 +128,9 @@ ENABLE_VENDOR_RIL_SERVICE := true
 TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_xiaomi
 TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2025-12-01
+
 # Sepolicy
 TARGET_SEPOLICY_DIR := msmsteppe
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
