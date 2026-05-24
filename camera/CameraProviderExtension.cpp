c5d2681cf1510a57ad1009f690b8f33950942929
chemusqui1 <jmportos@gmail.com>
2026-05-24 20:57:09 +0200

    sm6150-common: camera: Add support for tucana/toco soft lights
    We have 4 LEDs in total, the 2 of normal flashlights, used by default, and the 2 soft lights ximi wanted to include, using a custom controller for it. We'll benefit from this, more light
    
    Signed-off-by: chemusqui1 <jmportos@gmail.com>

--- camera/CameraProviderExtension.cpp
+++ camera/CameraProviderExtension.cpp
@@ -16,13 +16,22 @@ static std::string kTorchLedPaths[] = {
         "/sys/devices/platform/soc/c440000.qcom,spmi/spmi-0/spmi0-05/c440000.qcom,spmi:qcom,pm6150l@5:qcom,leds@d300/leds/led:torch_1",
 };
 
+// Soft light paths (toco and tucana only)
+static std::string kSoftLightPaths[] = {
+        "/sys/devices/platform/soc/890000.i2c/i2c-3/3-0063/leds/softlight_torch_0",
+        "/sys/devices/platform/soc/890000.i2c/i2c-3/3-0063/leds/softlight_torch_1",
+};
+
 /**
  * Write value to path and close file.
  */
 template <typename T>
 static void set(const std::string& path, const T& value) {
     std::ofstream file(path);
-    file << value;
+    // Avoid messing up all other sm6150 family
+    if (file.is_open()) {
+        file << value << std::endl; 
+    }
 }
 
 /**
@@ -70,8 +79,23 @@ void setTorchStrengthLevelExt(int32_t torchStrength, bool enabled) {
         auto node = path + "/" + TORCH_BRIGHTNESS;
         set(node, torchStrength);
     }
-    if (enabled)
+    
+    // Tucana and toco soft lights have a max_brightness value of 127,
+    // so set values to half for a balanced experience
+    int32_t softStrength = torchStrength / 2;
+    for (auto& path : kSoftLightPaths) {
+        auto node = path + "/" + TORCH_BRIGHTNESS;
+        set(node, softStrength);
+    }
+    if (enabled) {
         set(TOGGLE_SWITCH, 255);
+    // Make soft lights kick in too
+    } else {
+        for (auto& path : kSoftLightPaths) {
+            auto node = path + "/" + TORCH_BRIGHTNESS;
+            set(node, 0);
+        }
+    }
 }
 
 void setTorchModeExt(bool enabled) {
